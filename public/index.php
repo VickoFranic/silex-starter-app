e0b54632f1a2cc61c97ed7f53735974cf7ba59c6
vickofranic <vfranic@gmail.com>
2016-01-06 19:07:50 +0100

    Typo fix

--- public/index.php
+++ public/index.php
@@ -6,20 +6,20 @@
 *****************************************
 */
 
-	// Vendor autoloader
-	require __DIR__ . '/../vendor/autoload.php';
+// Vendor autoloader
+require __DIR__ . '/../vendor/autoload.php';
 
-	$app = new Silex\Application();
+$app = new Silex\Application();
 
-	// Custom config file - modify it to your needs
-	$config = include __DIR__ . '/../app/config/config.php';
+// Configuration file
+$config = include __DIR__ . '/../app/config/config.php';
 
-	//Register application routes in this file
-	include __DIR__ . '/../app/config/routes.php';
+// Application routes
+include __DIR__ . '/../app/config/routes.php';
 
-	// Get debug setup info from config
-	$app['debug'] = $config['debug'];
+// Get debug setup info from config
+$app['debug'] = $config['debug'];
 
-	// Run Silex app
-	$app->run();
+// Run Silex app
+$app->run();
 
