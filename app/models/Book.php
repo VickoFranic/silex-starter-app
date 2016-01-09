b10027ae2eb9a661a31555522b20a77ed9044dad
vickofranic <vfranic@gmail.com>
2016-01-09 20:56:31 +0100

    update

---
+++ app/models/Book.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace app\models;
+
+/**
+ * Example Book model class
+ */
+class Book
+{
+	/**
+	* @var integer
+	*/
+	public $id;
+
+	/**
+	 * @var string
+	 */
+	public $name;
+
+	/**
+	 * @var string
+	 */
+	public $author;
+
+}
