74611aad255fd2c78896dd03f1d5f7bebb63191c
vickofranic <vfranic@gmail.com>
2016-02-21 12:04:21 +0100

    Custom middleware created

---
+++ app/Middleware.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace app;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+ /**
+ * Custom application middleware
+ */
+ 
+ class Middleware
+ {
+ 	/**
+ 	 * User middleware. Redirect to index page if user not logged in
+ 	 */
+	function user(Request $req, Application $app)
+	{
+	 	if(! $app['session']->get('user')) {
+	 		return $app->redirect('/');
+	 	}
+	}
+
+ }
