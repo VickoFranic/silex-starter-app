151ad1bac32f072e46742be81ceb6db5aa592846
vickofranic <vfranic@gmail.com>
2016-02-21 12:03:13 +0100

    Middleware added

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,17 +1,23 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 /**
  **********************************
  * REGISTER APPLICATION ROUTES HERE
  **********************************
  */
 
+# Guest area
 $app->get('/', 'app\controllers\IndexController::index');
 $app->get('/login', 'app\controllers\IndexController::login');
 
-$app->get('/home', 'app\controllers\UserController::index');
+# User area
+$app->get('/home', 'app\controllers\UserController::index')->before('app\Middleware::user');
 $app->get('/home/pages', 'app\controllers\UserController::pages');
 
+# Admin area
+# TODO
 
 // Error handler
 $app->error(function(\Exception $e, $code) use ($app) {	
